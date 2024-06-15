e234e8093585eefffbb7f98dcc0bdbede0b9c8d5
Peter Kiptanui <kiptanui@amazon.com>
2024-06-15 10:48:16 +0300

    git push

---
+++ src/main/java/co/ke/safaricom/kiptanui/stringpadder/StringPadderFactory.java
@@ -0,0 +1,25 @@
+package co.ke.safaricom.kiptanui.stringpadder;
+
+/**
+ * Factory for creating instances of {@link StringPadder}.
+ */
+public final class StringPadderFactory {
+
+    private StringPadderFactory() {
+
+    }
+
+    /**
+     * Creates an instance of {@link StringPadder}.
+     *
+     * @return the new instance
+     */
+
+    public static StringPadder createStringPadder() {
+
+        return new StringPadderImpl();
+
+    }
+
+
+}
